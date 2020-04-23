2547796858f322f7cee3a54665be2c903da67822
mayur <mayur@DESKTOP-13IHC2O>
2020-04-23 13:05:11 +0530

    introduction to frames. drag and drop with actions class

---
+++ Introduction/src/frames/Switchto_Frame.java
@@ -0,0 +1,30 @@
+package frames;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Switchto_Frame 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "src//Driver//chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://jqueryui.com/droppable/");
+		driver.manage().window().maximize();
+		System.out.println(driver.findElements(By.tagName("iframe")).size());
+		driver.switchTo().frame(driver.findElement(By.className("demo-frame")));
+		WebElement source = driver.findElement(By.id("draggable"));
+				source.click();
+				WebElement target=driver.findElement(By.id("droppable"));
+				
+		Actions action=new Actions(driver);
+		action.dragAndDrop(source, target).build().perform();
+
+	}
+
+}
