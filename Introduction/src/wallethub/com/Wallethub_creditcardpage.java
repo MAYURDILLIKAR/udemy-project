245ef8bb5e61972d9256a54171dac83f5f800074
mayur <mayur@DESKTOP-13IHC2O>
2020-04-30 20:22:00 +0530

    mouse hover actions

---
+++ Introduction/src/wallethub/com/Wallethub_creditcardpage.java
@@ -0,0 +1,36 @@
+package wallethub.com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class Wallethub_creditcardpage
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "src\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://wallethub.com/");
+		System.out.println(driver.getTitle());
+
+		WebElement creditcard_link = driver.findElement(By.linkText("Credit Cards"));
+		System.out.println(creditcard_link.getText());
+		String fount_size= creditcard_link.getCssValue("font-size");
+		System.out.println("the header fount size is"+ creditcard_link);
+		//creditcard_link.click();
+		//WebElement move=driver.findElement(By.linkText("Credit Cards"));
+
+		Actions a=new Actions(driver);
+		a.moveToElement(creditcard_link).build().perform();
+		//a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+		//a.moveToElement(move).contextClick().build().perform();
+
+	}
+
+}
